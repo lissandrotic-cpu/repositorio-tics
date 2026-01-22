697aaab963202668a84271a43f1cfbeac3248a5a
lissandrotic-cpu <lissandro.tic@gmail.com>
2026-01-22 09:39:47 -0600

    Add files via upload

---
+++ lissandro/tic 6/web/JAVASCRIPT/script.js
@@ -0,0 +1,7 @@
+//obtener el boton por su ID que se puso 
+const boton1=document.getElementById('tilin');
+
+//agregar un evento al boton 
+boton1.addEventListener('click',function(){
+	alert("YEAHH");
+});
