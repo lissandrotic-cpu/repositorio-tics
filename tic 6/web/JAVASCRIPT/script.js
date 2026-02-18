d6994eebc51955e9a5f333dfca1d620b55955048
lissandrotic-cpu <lissandro.tic@gmail.com>
2026-02-18 09:22:04 -0600

    Add files via upload

---
+++ tic 6/web/JAVASCRIPT/script.js
@@ -0,0 +1,15 @@
+//obtener el boton por su ID que se puso 
+const boton1=document.getElementById('tilin');
+
+//agregar un evento al boton 
+boton1.addEventListener('click',function(){
+	alert("ARRIBA EL AMERICA");
+});
+function toggleMobileMenu() {
+    const menu = document.getElementById("mobileMenu");
+    if (menu.style.width === "100%") {
+        menu.style.width = "0";
+    } else {
+        menu.style.width = "100%";
+    }
+}
